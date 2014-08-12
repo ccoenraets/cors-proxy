c5fdacd242eec475325a2da5a76b65100650e969
ccoenraets <ccoenraets@gmail.com>
2014-08-12 17:55:11 -0400

    Fixed Target-URL header

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ app.all('*', function (req, res, next) {
 
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
         res.send();
     } else {
-        var url = req.header('Target-Endpoint');
+        var targetURL = req.header('Target-URL');
         if (!url) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
-        request({ url: url, method: req.method, qs: req.query, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
